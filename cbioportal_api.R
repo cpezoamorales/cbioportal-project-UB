3e162d17c6346ec6fe48b7607facf537f6c37d67
ljovalramentol <ljoval@vhio.net>
2024-06-12 19:31:44 +0200

    He añadido la explicación en #

--- cbioportal_api.R
+++ cbioportal_api.R
@@ -4,30 +4,31 @@ library(dplyr)
 
 ## Link: https://www.karissawhiting.com/cbioportalR/articles/overview-of-workflow.html
 
-set_cbioportal_db("public")
-test_cbioportal_db()
+set_cbioportal_db("public") #conectarnos a cbioportal
+test_cbioportal_db() #testar que estamos conectados
 
 
-all_studies <- available_studies()
+all_studies <- available_studies() #descargarnos todos los estudios
 all_studies
 
 
 get_study_info("brca_tcga_pan_can_atlas_2018") %>%
-  t()
+  t() #Ver la información del estudio que nos estamos descargando
 
-available_profiles(study_id = "brca_tcga_pan_can_atlas_2018")
+available_profiles(study_id = "brca_tcga_pan_can_atlas_2018") #ver tipo de archivos que hay
 
 available_profiles(study_id = "brca_tcga_pan_can_atlas_2018") %>%
-  pull(molecularProfileId)
+  pull(molecularProfileId) #ver archivos que hay en molecular profile
 
 
-genetics <- get_genetics_by_study(study_id = "brca_tcga_pan_can_atlas_2018")
+genetics <- get_genetics_by_study(study_id = "brca_tcga_pan_can_atlas_2018") # Get All Genomic Information By Study
 
-attr_brca <- available_clinical_attributes("brca_tcga_pan_can_atlas_2018")
+attr_brca <- available_clinical_attributes("brca_tcga_pan_can_atlas_2018") #Get all available clinical attribute IDs for a study
 
 
-p1 <- available_patients("brca_tcga_pan_can_atlas_2018")
+p1 <- available_patients("brca_tcga_pan_can_atlas_2018") #Get All Patient IDs in a Study
 
 clinical_brca <- get_clinical_by_patient(patient_id = p1$patientId,
                                          study_id = "brca_tcga_pan_can_atlas_2018")
 
+head(clinical_brca)
