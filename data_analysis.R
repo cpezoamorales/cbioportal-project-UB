7ea51de9ace3460dae0dd37030e723da42e88a74
Cesar Pezoa <pezoamorales@gmail.com>
2024-06-13 22:13:37 -0400

    revisión código y avance en correlación de datos clinicos y genéticos

--- data_analysis.R
+++ data_analysis.R
@@ -1,18 +1,19 @@
+###--------------------------------------------------------------------------###
+# Código para el análisis de datos descargados desde cbioportal
+###--------------------------------------------------------------------------###
 
-library(cbioportalR)
-library(dplyr)
-library (tidyverse)
-library(purrr)
-library(gtsummary)
-library(survival)
-library(survminer)
-library(ggsurvfit)
+### Intalación de librerias necesarias para el análisis:
+if(!require("cbioportalR")){install.packages("cbioportalR")}
+if(!require("tidyverse")){install.packages("tidyverse")}
+if(!require("gtsummary")){install.packages("gtsummary")}
+if(!require("survival")){install.packages("survival")}
+if(!require("survminer")){install.packages("survminer")}
+if(!require("ggsurvfit")){install.packages("ggsurvfit")}
+if(!require("corrplot")){install.packages("corrplot")}
+if(!require("caret")){install.packages("caret")}
 
-
-
-# Load data, this format is faster thar read from the cbioportal API
-load("C:/Users/ljoval/OneDrive - VHIO/LAIA/CURSOS/UB_DATA_SCIENCE/TREBALL FINAL/cbioportal-project-UB/data.RData")
-
+# Carga de datos crusdos almacenados previamente en archivo RData
+load("data.RData")
 
 # Transformar la tabla en Wide con pivot_wider, crea columnas viniendo de muchas filas
 clinical_brca_wide <- clinical_brca %>%
@@ -25,7 +26,6 @@ length(unique(clinical_brca$patientId))
 
 
 #Transformar variables numericas a numericas y factores
-
 convert_type <- function(column, type) {
   if (type == "NUMBER") {
     return(as.numeric(column))
@@ -44,12 +44,11 @@ for (i in seq_len(nrow(attr_brca))) {
   if (column_name %in% names(clinical_brca_wide)) {
     clinical_brca_wide[[column_name]] <- convert_type(clinical_brca_wide[[column_name]], column_type)
   }
-  
 }
 
-
+###--------------------------------------------------------------------------###
 # Analisis descriptivo de las variables
-
+###--------------------------------------------------------------------------###
 # Crear resumen de tabla excluyendo las columnas uniquePatientKey, patientId y studyId
 tbl <- tbl_summary(
   clinical_brca_wide %>%
@@ -66,19 +65,14 @@ tbl <- tbl_summary(
 tbl
 
 # Cambiar los niveles de la variable PFS_STATUS
-clinical_brca_wide$PFS_STATUS <- factor(clinical_brca_wide$PFS_STATUS, levels = c("0:CENSORED", "1:PROGRESSION"), labels = c("0", "1"))
+clinical_brca_wide$PFS_STATUS <- factor(clinical_brca_wide$PFS_STATUS, 
+                                        levels = c("0:CENSORED", "1:PROGRESSION"), 
+                                        labels = c("0", "1"))
 
 # Verificar los nuevos niveles
 levels(clinical_brca_wide$PFS_STATUS)
 
-
-
-
 #ALTERNATIVA paquete  "ggsurvfit" --> mejor este se puede editar con ggplot2
-
-
-#> Loading required package: ggplot2
-
 p <- survfit2(Surv(PFS_MONTHS, PFS_STATUS == "1") ~ 1, data = clinical_brca_wide) %>%
   ggsurvfit(linewidth = 1, color = "#607B8B") +  # Cambiar el color de la línea
   add_confidence_interval(fill = "#B0E2FF") +  # Cambiar el color del sombreado
@@ -90,23 +84,107 @@ p <- survfit2(Surv(PFS_MONTHS, PFS_STATUS == "1") ~ 1, data = clinical_brca_wide
     x= " Time (months)",
     title = "PFS"
   )
-
 p
 
-
-tbl_survfit(pfs, times=c(60, 120),label_header = "% Survival at Time {time} (95% CI)")
-
-#Alternativa de PFS con subgrupos
+### Alternativa de PFS con subgrupos
 
 # Crear un objeto de supervivencia en función de SUBTYPE --> se tendría que cambiar a la función de arriba para tenerlo todo igual
-
 pfs <- survfit(Surv(PFS_MONTHS, PFS_STATUS == "1") ~ SUBTYPE, data = clinical_brca_wide)
 
 #PFS_STATUS == "1" es per generar un vector TRUE, FALSE i que agafi d'event el TRUE
-
 pfs
 
+tbl_survfit(
+  pfs, 
+  times=c(60, 120),
+  label_header = "% Survival at Time {time} (95% CI)"
+  )
+
 # Crear el gráfico de supervivencia
-ggsurvplot(pfs, data = clinical_brca_wide, risk.table = TRUE, legend.title="", title= "PFS", xlab="Time (months)", ylab= "Progression Free Survival (PFS)")
+ggsurvplot(pfs, 
+           data = clinical_brca_wide, 
+           risk.table = TRUE, legend.title="",
+           title= "PFS",
+           xlab="Time (months)",
+           ylab= "Progression Free Survival (PFS)")
 
 
+### Análisis exploratorios: Correlación de variables numéricas
+clinical_brca_numeric <- clinical_brca_wide %>% 
+  select_if(is.numeric)
+
+# lower case:
+names(clinical_brca_numeric) <- c("age", "buffa_hypoxia_score", "days_last_followup",
+                                  "days_birth", "days_pathologic_diag", "dfs_month",
+                                  "dss_month", "os_month", "pfs_month", 
+                                  "ragnum_hypoxia_score", "winter_hypoxia_score")
+
+pairs(clinical_brca_numeric)
+plot(clinical_brca_numeric$days_pathologic_diag)
+
+clinical_brca_numeric_2 <- clinical_brca_numeric
+clinical_brca_numeric_2$days_pathologic_diag <- NULL
+pairs(clinical_brca_numeric_2)
+
+clinical_corr <- cor(clinical_brca_numeric_2)
+corrplot(clinical_corr, type = "upper")
+# problema al hacer correlación, quizás por distinto rango de los valores
+# Normalizar los datos?
+
+par(mfrow=c(1,3))
+boxplot(clinical_brca_numeric$age)
+hist(clinical_brca_numeric$age)
+plot(clinical_brca_numeric$age, pch=20)
+
+boxplot(clinical_brca_numeric$winter_hypoxia_score)
+hist(clinical_brca_numeric$winter_hypoxia_score)
+hist(clinical_brca_numeric$ragnum_hypoxia_score)
+hist(clinical_brca_numeric$buffa_hypoxia_score)
+hist(clinical_brca_numeric$dfs_month)
+
+###--------------------------------------------------------------------------###
+### Analisis de expresión de genes con WGCNA
+###--------------------------------------------------------------------------###
+# Instalar Bioconductor
+#if (!require("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install(version = "3.19")
+
+# Instalar y cargar el paquete WGCNA
+#BiocManager::install("WGCNA")
+library("WGCNA")
+
+# Lectura de datos
+rna_data <- read_csv2("data_mrna_seq_v2_rsem.csv")
+col_select <- rna_data[, -1]
+
+# Transformación de tabla para el análisis
+rna_longer <- t(col_select)
+colnames(rna_longer) <- rna_longer[1,]
+rna_longer <- rna_longer[-1, ]
+
+# look for missing values
+gsg = goodSamplesGenes(rna_longer, verbose = 3);
+gsg$allOk
+
+sampleTree <- hclust(dist(rna_longer), method = "average")
+plot(sampleTree)
+
+clust = cutreeStatic(sampleTree, cutHeight = 31000, minSize = 10)
+
+
+# Normalización de las lecturas 
+BiocManager::install("DESeq2")
+library(DESeq2)
+
+de_input <- as.matrix(rna_longer)
+
+meta_df <- data.frame( Sample = names(rna_data[-1])) %>%
+  mutate(
+    Type = gsub("-.*","", Sample) %>% gsub("[.].*","", .)
+  )
+
+dds <- DESeqDataSetFromMatrix(round(de_input),
+                              meta_df,
+                              design = ~Type)
+enableWGCNAThreads()
